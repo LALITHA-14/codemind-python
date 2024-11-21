68bdba09f6334f8b23cda24ffa2910079fc6c2ef
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-11-21 12:51:41 +0530

    import math

---
+++ Return_the_Change.py
@@ -0,0 +1,13 @@
+# cook your dish here
+import math
+t = int(input())
+for i in range(t):
+    n = int(input())
+    if(n%10==0):
+        print(100-n)
+    else:
+        if(n%10>=5):
+            rounded_cost = n+(10-(n%10)) # Round up
+        else:
+            rounded_cost = n-(n%10) # Round down
+        print(100-rounded_cost)
