98b4983bb5897046331dfd3740ac4c6d75965f2b
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-02-06 18:07:25 +0530

    t=int(input())

---
+++ Which_Division.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    r=int(input())
+    if(2000<=r):
+        print("1")
+    elif(1600<=r<2000):
+        print("2")
+    else:
+        print("3")
