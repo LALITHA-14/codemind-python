37088ffb007db3e287ac964064e0c0acf91d05ea
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-06-11 21:13:19 +0530

    def insertAtEnd(arr ,sizeOfArray ,element):

---
+++ Array_insert_at_end.py
@@ -0,0 +1,7 @@
+# You only need to insert the given element at 
+# the end, i.e., at index sizeOfArray - 1. You may 
+# assume that the array already has sizeOfArray - 1
+# elements. 
+def insertAtEnd(arr,sizeOfArray,element):
+    ##Your code here
+    return arr.append(element)
