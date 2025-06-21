30386a154df67095f1e7b12da89ce4308a1928fa
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-06-21 17:15:46 +0530

    class Solution:

---
+++ Reverse_Words.py
@@ -0,0 +1,8 @@
+# User function Template for python3
+
+class Solution:
+    # Function to reverse words in a given string.
+    def reverseWords(self, s):
+        # code here 
+        return ' '.join(s.split()[::-1])
+        
