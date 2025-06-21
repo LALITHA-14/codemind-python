355d88cf55c8882965849699cf67d5a69c9dbebd
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-06-21 17:42:43 +0530

    class Solution:

---
+++ Multiply_Strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def multiplyStrings(self, s1, s2):
+        # code here
+        # return the product string
+        first=(int(s1))
+        second=(int(s2))
+        return first*second
