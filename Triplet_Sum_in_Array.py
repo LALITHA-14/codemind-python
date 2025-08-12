a2d5bc5dc14208288507a3417821d0e3166402d9
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-08-12 19:08:37 +0530

    class Solution:

---
+++ Triplet_Sum_in_Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasTripletSum(self, arr, target):
+        # Code Here
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                for k in range(j+1,len(arr)):
+                    if arr[i]+arr[j]+arr[k]==target:
+                        return True
+        return False
