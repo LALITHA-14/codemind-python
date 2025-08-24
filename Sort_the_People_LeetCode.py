98eda6cb4646b1c3c436596476425e4fe8c93cd0
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-08-24 23:17:32 +0530

    from typing import List

---
+++ Sort_the_People_LeetCode.py
@@ -0,0 +1,6 @@
+from typing import List
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        people=list(zip(heights,names))
+        people.sort(reverse=True)
+        return [name for _,name in people]
