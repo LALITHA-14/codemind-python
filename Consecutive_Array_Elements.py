207f028f89728eb6df8074f6ef5d2aef6711ce9a
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-10-27 12:17:09 +0530

    class Solution:

---
+++ Consecutive_Array_Elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def areConsecutives(self, arr):
+        # Your code goes here
+        arr.sort()
+        for i in range(len(arr)-1):
+            if arr[i+1]-arr[i]!=1:
+                return False
+        return True
