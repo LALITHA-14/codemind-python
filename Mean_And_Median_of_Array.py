3247e2cbebd01d1bdb99c15de325cbcd1da62fe8
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-12-21 23:04:11 +0530

    import numpy as np

---
+++ Mean_And_Median_of_Array.py
@@ -0,0 +1,8 @@
+import numpy as np
+class Solution:
+    def median(self, arr):
+        #code here
+        return int(np.median(arr))
+    def mean(self, arr):
+        #code here
+        return int(np.mean(arr))
