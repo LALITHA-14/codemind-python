3072e6c8fe1efd34832b140c92d50040cbfc22f3
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2026-02-09 22:08:30 +0530

    class Solution:

---
+++ EvenOdd.py
@@ -0,0 +1,9 @@
+class Solution:
+    def evenOdd(self, a, b):
+        #Code here
+        if a%2==0:
+            print(a,end="\n")
+            print(b,end="\n")
+        elif b%2==0:
+            print(b,end="\n")
+            print(a,end="\n")
